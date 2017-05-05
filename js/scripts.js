3ffb604c55a19a77aeb99554c2e8d3aa8fb13629
Jared Eiseman <jathei@gmail.com>
2017-05-05 10:19:11 -0700

    add comments and re-order scripts.js business logic functions to order reflected in pingPong()

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
-var replaceFifteens = function(input) {
-  var result = input.map(function(element, index) {
-    if ((index + 1) % 15 === 0) {
-      return "ping-pong";
-    }
-    return element;
-  })
+////////////////////////////////////////////////////////////////////////////////
+//                              BUSINESS LOGIC                                //
+////////////////////////////////////////////////////////////////////////////////
+
+//Run business logic
+var pingPong = function(input, order) {
+  var output;
+  if (!validate(input)) {
+    return false;
+  }
+  output = count(input);
+  output = replaceThrees(output);
+  output = replaceFives(output);
+  output = replaceFifteens(output);
+  if (order === -1) {
+    output = output.reverse();
+  }
+  return output;
+};
+
+//Validate the entered number is a positive integer
+var validate = function(input) {
+  if (input > 0) {
+    return true;
+  }
+  return false;
+};
+
+//Count up to the provided number
+var count = function(input) {
+  var result = [];
+  for (var i = 1; i <= input; i++) {
+    result.push(i);
+  }
   return result;
 };
 
-var replaceFives = function(input) {
-  var result = input.map(function(element, index) {
-    if ((index + 1) % 5 === 0) {
-      return "pong";
-    }
-    return element;
-  })
-  return result;
-};
-
+//Replace all numbers divisible by 3 with "ping"
 var replaceThrees = function(input) {
   var result = input.map(function(element, index) {
     if ((index + 1) % 3 === 0) {
@@ -28,39 +46,32 @@ var replaceThrees = function(input) {
   return result;
 };
 
-var count = function(input) {
-  var result = [];
-  for (var i = 1; i <= input; i++) {
-    result.push(i);
-  }
+//Replace all numbers divisible by 5 with "pong"
+var replaceFives = function(input) {
+  var result = input.map(function(element, index) {
+    if ((index + 1) % 5 === 0) {
+      return "pong";
+    }
+    return element;
+  })
   return result;
 };
 
-var validate = function(input) {
-  if (input > 0) {
-    return true;
-  }
-  return false;
+//Replace all numbers divisible by 15 with "ping-pong"
+var replaceFifteens = function(input) {
+  var result = input.map(function(element, index) {
+    if ((index + 1) % 15 === 0) {
+      return "ping-pong";
+    }
+    return element;
+  })
+  return result;
 };
 
-var pingPong = function(input, order) {
-  var output;
 
-  if (!validate(input)) {
-    return false;
-  }
-
-  output = count(input);
-  output = replaceThrees(output);
-  output = replaceFives(output);
-  output = replaceFifteens(output);
-
-  if (order === -1) {
-    output = output.reverse();
-  }
-
-  return output;
-};
+////////////////////////////////////////////////////////////////////////////////
+//                           USER INTERFACE LOGIC                             //
+////////////////////////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
   $('#user-input').submit(function(e) {
@@ -72,6 +83,7 @@ $(document).ready(function() {
     var result = pingPong(input, order);
     //Clear existing results from results div
     $('#results ul li').remove();
+    //Display error message when validation fails
     if (!result) {
       $('#results ul').append('<li>The number you provided is invalid, please enter a positive integer.</li>');
       return;
