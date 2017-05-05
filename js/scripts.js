0908e97d2e67d783fed0c06b78bfb604b5186c19
Jared Eiseman <jathei@gmail.com>
2017-05-05 08:38:34 -0700

    add validate function to verify entered input is greater than 0, add error message to UI logic if validate fails

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
+var validate = function(input) {
+  if (input > 0) {
+    return true;
+  }
+  return false;
+}
+
 var pingPong = function(input) {
+  if (!validate(input)) {
+    return false;
+  }
   return input;
 };
 
@@ -9,6 +19,11 @@ $(document).ready(function() {
     var input = $('input[name="number"]').val();
     //Call business logic to process input
     var result = pingPong(input);
+    if (!result) {
+      $('#results ul li').remove();
+      $('#results ul').append('<li>The number you provided is invalid, please enter a positive integer.</li>');
+      return;
+    }
     //Display result to screen
     $('#results ul').append('<li>' + result + '</li>');
   });
